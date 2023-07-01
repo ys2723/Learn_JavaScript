e4f421bc87b9c17647663b8e911360adbf00ebad
Yash Tripathi <yashtripathi2205@gmail.com>
2023-07-01 21:54:00 +0530

    Initial Commit

---
+++ 1-->Basic_Codes.js
@@ -0,0 +1,12 @@
+console.log("Yugoslavia is no more")     // This would print in concole, not on the main page
+document.write("I know, it's sad")       // This would print on the MAIN page
+
+window.alert("ARE YOU A COMMUNIST???")   // This is a pop up alert
+
+// Single line comment 
+
+/*
+    Multi
+    Line 
+    Comment
+*/
